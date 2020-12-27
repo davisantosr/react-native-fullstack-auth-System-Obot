4f23562a2fdcd04c082789eda2359d89842a79a4
davisantosr <davi-rocha2@hotmail.com>
2020-12-27 16:28:50 -0300

    add express | add Routes | initial routes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const authRoutes = require('./routes/auth')
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to the auth system')
 
 })
+
+app.use('/api/users', authRoutes);
 
 app.listen(5000, () => console.log('Server is running on port 5000'))
